11d8fe1072571a4cd3cdf44bcfe40d444649897d
amyspencerproject <amy@spencerproject.com>
2022-03-28 15:52:41 -0600

    Added collections for work and featured work. Made a new directory called utils for a sorting function used for both collections

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,18 @@
+const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
+
 module.exports = config => {
+  // Returns work items, sorted by display order
+  config.addCollection('work', collection => {
+    return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md'));
+  });
+
+  // Returns work items, sorted by display order then filtered by featured
+  config.addCollection('featuredWork', collection => {
+    return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md'))
+      .filter(x => x.data.featured
+    );
+  });
+
   // Set directories to pass through to the dist folder
   config.addPassthroughCopy('./src/images/');
     return {
@@ -8,6 +22,6 @@ module.exports = config => {
       dir: {
         input: 'src',
         output: 'dist'
-      }
-    };
+    }
+  };
 };
